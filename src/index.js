3e54abc8a7336f4a0610b4312271823a3596fe21
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-11 23:27:29 +0100

    PORT hotfix

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import requireAuth from "./middlewares/requireAuth.js";
 import { AuthRoutes } from "./routes/authRoutes.js";
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
