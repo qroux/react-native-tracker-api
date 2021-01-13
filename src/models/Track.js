6a1710f2cd5e417d59ab8b618659905ddb85a39f
Quentin Roux <quentin.roux@hotmail.fr>
2021-01-13 19:18:39 +0100

    Track schema

---
+++ src/models/Track.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+
+// CREATE the Schema
+const pointSchema = new mongoose.Schema({
+  timestamp: Number,
+  coords: {
+    latitude: Number,
+    longitude: Number,
+    altitude: Number,
+    accuracy: Number,
+    heading: Number,
+    speed: Number,
+  },
+});
+
+const trackSchema = new mongoose.Schema({
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+  name: {
+    type: String,
+    default: "",
+  },
+  locations: [pointSchema],
+});
+
+// ASSOCIATE the schema with mongoose.model
+mongoose.model("Track", trackSchema);
+
+// NB, no export needed, only used Once
